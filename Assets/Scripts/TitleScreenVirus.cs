1d39dadb94a72d92062f1f0b8143d16439678d90
Robert May <robertmay@Roberts-iMac.local>
2020-06-04 09:09:12 -0400

    Added .blend for assets

--- Assets/Scripts/TitleScreenVirus.cs
+++ Assets/Scripts/TitleScreenVirus.cs
@@ -8,16 +8,28 @@ public class TitleScreenVirus : MonoBehaviour
     public float speed;
     public float waveAmplitude;
     public float lifetime = 10f;
+
+    private float _spawnTime;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        _spawnTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(
+            Mathf.Sin(Time.time) * waveAmplitude,
+            speed * Time.deltaTime,
+            0,
+            Space.World
+        );
+
+        if (Time.time - _spawnTime > lifetime)
+        {
+            Destroy(gameObject);
+        }
     }
 }
